d2bff80ab7514bbdeba7e2e2d6fb1ba90bb58fea
lmendiboure <leo.mendiboure11@gmail.com>
2023-02-09 10:05:15 +0100

    Docker Install

---
+++ Docker/install.sh
@@ -0,0 +1,10 @@
+apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+
+apt update
+apt install docker-ce
+systemctl enable docker
+
+# Next line could be added to avoid using sudo 
+# addgroup yourLogin docker
