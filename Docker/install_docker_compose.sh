a1eea83e14594578f4f0d3329de83462ccaa7071
lmendiboure <leo.mendiboure11@gmail.com>
2023-09-26 14:24:48 +0200

    Update install_docker_compose.sh

--- Docker/install_docker_compose.sh
+++ Docker/install_docker_compose.sh
@@ -1,4 +1,4 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/ docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
